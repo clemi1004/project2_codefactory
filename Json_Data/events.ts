150f3ad652693d0908ad81fc19e52705494ef375
Clemens Sauer <clemens.sauer@gmx.net>
2019-10-30 14:28:36 +0100

    new updates

--- Json_Data/events.ts
+++ Json_Data/events.ts
@@ -1,13 +1,13 @@
 // loop for adding html div
-for (let i = 0; i < careers.length; i++)
+for (let i = 0; i < events.length; i++)
       {
          // definition of the new element
          var newItem_1 = 
         	`<div class="parent" onclick="">
-                <div class="child" style="background-image: url('`+ careers[i].img +`');">
-                    <p class="header">`+ careers[i].type_of_event +`</p>
-                    <p class="date">Date: `+ careers[i].date +`<br>Location: `+ careers[i].location +`</p>
-                    <p class="description">`+ careers[i].description +`</p>
+                <div class="child" style="background-image: url('`+ events[i].img +`');">
+                    <p class="header">`+ events[i].type_of_event +`</p>
+                    <p class="date">Date: `+ events[i].date +`<br>Location: `+ events[i].location +`</p>
+                    <p class="description">`+ events[i].description +`</p>
                 </div>
             </div>`
          // insert the new element  
